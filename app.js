2acac15bc4c07ecee690f232fa88e964fb6779cf
Ana Freitas <ana.freitaas16@gmail.com>
2022-08-09 09:38:48 -0300

    creating a post request on postman

--- app.js
+++ app.js
@@ -36,8 +36,24 @@ app.get("/articles" , function (req, res){
         } else {
             res.send(err)
         }
-    })
-})
+    });
+});
+
+app.post("/articles" , function(req,res) {
+
+    const NewArticle = new Article ({
+        title: req.body.title ,
+        content: req.body.content
+    });
+
+    NewArticle.save( function(err){
+        if (!err) {
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });  
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
